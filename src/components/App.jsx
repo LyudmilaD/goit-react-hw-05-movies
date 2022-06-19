f000e19ae987664a93cccdec7d0df990ea04ac9e
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-06-19 19:40:18 +0300

    set react router dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,33 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import styles from './container.module.css';
+//import { Navigation } from "./Navigation/Navigation";
+//import { HomePage } from './HomePage/HomePage';
+//import { MoviesPage } from './MoviesPage/MoviesPage';
+//import { MovieDetailsPage } from './MovieDetailsPage/MovieDetailsPage';
+
+const HomePage = lazy(() => import('./HomePage/HomePage'));
+const MoviesPage = lazy(() => import('./MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() =>
+  import('./MovieDetailsPage/MovieDetailsPage')
+);
+const Navigation = lazy(() => import('./Navigation/Navigation'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div className={styles.App}>
+      <Navigation />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="movies" element={<MoviesPage />}></Route>
+          <Route
+            path="movies/:movieId/*"
+            element={<MovieDetailsPage />}
+          ></Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
