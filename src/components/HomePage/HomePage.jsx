6ca0f757134cf1cda55e295c7f6f11b5456dfe31
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-06-19 21:10:36 +0300

    url homepage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,1 +1,9 @@
-const API_KEY = '53cd46930a5f93ca31acedee3ad67268';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './HomePage.module.css';
+
+const HomePage = () => {
+  const BASE_URL = 'https://api.themoviedb.org/3';
+  const API_KEY = '53cd46930a5f93ca31acedee3ad67268';
+  const [popularFilms, setPopularFilms] = useState([]);
+};
